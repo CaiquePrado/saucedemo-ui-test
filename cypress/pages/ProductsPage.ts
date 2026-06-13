2d78c0ad53dda3672b3450edbe9a2c142dc424c7
CaiquePrado <caiqueprado76@gmail.com>
2026-06-13 17:39:09 -0300

    feat: adicionando product page com seletores

--- cypress/pages/ProductsPage.ts
+++ cypress/pages/ProductsPage.ts
@@ -1,15 +1,19 @@
-export function ProdcutsPage() {
+export function ProductsPage() {
   const selectors = {
     productsTitle: '[data-test="title"]',
     inventoryItems: '[data-test="inventory-item"]',
     itemName: '[data-test="inventory-item-name"]',
-    itemPrice: '[data-test="inventory-item-price"]'
+    itemPrice: '[data-test="inventory-item-price"]',
+    sortDropdown: '[data-test="product-sort-container"]'
   }
 
   const getInventoryItems = () => cy.get(selectors.inventoryItems)
   const getProductsTitle = () => cy.get(selectors.productsTitle)
+  const getSortDropdown = () => cy.get(selectors.sortDropdown)
+
   const getItemName = (item: JQuery<HTMLElement>) =>
     cy.wrap(item).find(selectors.itemName)
+
   const getItemPrice = (item: JQuery<HTMLElement>) => {
     return cy.wrap(item).find(selectors.itemPrice)
   }
@@ -18,6 +22,7 @@ export function ProdcutsPage() {
     getInventoryItems,
     getItemName,
     getItemPrice,
-    getProductsTitle
+    getProductsTitle,
+    getSortDropdown
   }
 }
