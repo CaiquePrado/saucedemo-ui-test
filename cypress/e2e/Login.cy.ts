18fbc2c364f57a04c921461f3537af10335ef740
CaiquePrado <caiqueprado76@gmail.com>
2026-06-13 17:39:57 -0300

    test: adicionando teste de usuário inválido

--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -1,12 +1,12 @@
 import { faker } from "@faker-js/faker"
 import "cypress-axe"
 import { LoginPage } from "../pages/LoginPage"
-import { ProdcutsPage } from "../pages/ProductsPage"
+import { ProductsPage } from "../pages/ProductsPage"
 import { getCredentials } from "../utils/Utils"
 
 describe("Login", () => {
   let loginPage: ReturnType<typeof LoginPage>
-  const productPage = ProdcutsPage()
+  const productPage = ProductsPage()
 
   const invalidUserName = faker.person.fullName()
   const invalidPassword = faker.internet.password()
@@ -23,7 +23,7 @@ describe("Login", () => {
   })
 
   it("should display an error message when the username is invalid", () => {
-    cy.login({ username: invalidUserName, password: "secret_sauce" })
+    cy.login({ username: invalidUserName, password: invalidPassword })
 
     loginPage
       .getErrorMessage()
@@ -33,51 +33,4 @@ describe("Login", () => {
         "Epic sadface: Username and password do not match any user in this service"
       )
   })
-
-  it("should display an error message when the password is invalid", () => {
-    cy.login({ username: "standard_user", password: invalidPassword })
-
-    loginPage
-      .getErrorMessage()
-      .should("be.visible")
-      .and(
-        "contain.text",
-        "Epic sadface: Username and password do not match any user in this service"
-      )
-  })
-
-  it("should display an error message when the username is not provided", () => {
-    cy.login({ password: "secret_sauce" })
-
-    loginPage
-      .getErrorMessage()
-      .should("be.visible")
-      .and("contain.text", "Epic sadface: Username is required")
-  })
-
-  it("should display an error message when the password is not provided", () => {
-    cy.login({ username: "standard_user" })
-
-    loginPage
-      .getErrorMessage()
-      .should("be.visible")
-      .and("contain.text", "Epic sadface: Password is required")
-  })
-
-  it("should display an error message when both username and password are not provided", () => {
-    cy.login({ username: "", password: "" })
-
-    loginPage
-      .getErrorMessage()
-      .should("be.visible")
-      .and("contain.text", "Epic sadface: Username is required")
-  })
-
-  // it("deve validar a acessibilidade da página de login", () => {
-  //   cy.visit("/")
-
-  //   cy.injectAxe()
-
-  //   cy.checkA11y(undefined, undefined, terminalLog)
-  // })
 })
